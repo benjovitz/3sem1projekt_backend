f1f5dac7a6d776149ed60be7f5c9c5773e886d3a
jaco718v <jaco718v@stud.kea.dk>
2023-03-22 18:24:59 +0100

    misc

--- src/main/java/dat3/voximovies/configuration/SetupDevUsers.java
+++ src/main/java/dat3/voximovies/configuration/SetupDevUsers.java
@@ -2,10 +2,7 @@ package dat3.voximovies.configuration;
 
 import dat3.security.entity.Role;
 import dat3.security.entity.UserWithRoles;
-import dat3.voximovies.entity.Showing;
-import dat3.voximovies.entity.User;
 import dat3.voximovies.repository.ShowingRepository;
-import dat3.voximovies.repository.UserRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
@@ -19,16 +16,12 @@ public class SetupDevUsers implements ApplicationRunner {
 
     UserWithRolesRepository userWithRolesRepository;
     String passwordUsedByAll;
-
-    UserRepository userRepository;
-
     ShowingRepository showingRepository;
 
 
 
-    public SetupDevUsers(UserWithRolesRepository userWithRolesRepository, UserRepository userRepository, ShowingRepository showingRepository) {
+    public SetupDevUsers(UserWithRolesRepository userWithRolesRepository, ShowingRepository showingRepository) {
         this.userWithRolesRepository = userWithRolesRepository;
-        this.userRepository=userRepository;
         this.showingRepository = showingRepository;
         passwordUsedByAll = "test12";
     }
@@ -61,10 +54,5 @@ public class SetupDevUsers implements ApplicationRunner {
         userWithRolesRepository.save(user2);
         userWithRolesRepository.save(user3);
         userWithRolesRepository.save(user4);
-
-        User u1 = new User("member1","test12", "dooo", "Benjamin Buttom","22222","kik 12","Smirum","3333");
-        Showing s1 = new Showing(200,new ArrayList<>(),150, LocalDateTime.now());
-        userRepository.save(u1);
-        showingRepository.save(s1);
     }
 }
